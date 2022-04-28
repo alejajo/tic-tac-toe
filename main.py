4af3feac60d7d7b7e3226f9b4f9d416b64c9bc46
alejajo <103969184+alejajo@users.noreply.github.com>
2022-04-28 09:14:44 -0600

    Implementación del juego 3 en raya

--- main.py
+++ main.py
@@ -17,18 +17,68 @@ def print_board(board):
     print('\n'.join([str(row) for row in board]))
 
 
-def update_board(board, position, player):
-    # TODO: modificar tablero con nuevo movimiento del jugador en posicion indicada.
+def update_board(board, x, y,  player):
+    board[x][y]=str (player) 
     pass
 
 
-def check_for_winner(board, player):
-    # TODO: evaluar si el jugador indicado ha ganado la partida.
-    pass
+def check_for_winner(board):
+    if board[1][0] and board[1][1] and board[1][2] or board[2][0] and board[2][1] and board[2][2] or board[0][0] and board[0][1] and board[0][2] :
+        print("-----------------------------")
+        print("-----------------------------")
+        print("-----------------------------")
+        print("    HAS GANADO EL JUEGO   ")
+        print("-----------------------------")
+        print("-----------------------------")
+        print("-----------------------------")
+
+    if board[0][1] and board[0][0] and board[0][2] or board[1][1] and board[1][0] and board[1][2] or board[2][1] and board[2][0] and board[2][2]:
+        print("-----------------------------")
+        print("-----------------------------")
+        print("-----------------------------")
+        print("    HAS GANADO EL JUEGO   ")
+        print("-----------------------------")
+        print("-----------------------------")
+        print("-----------------------------")
+    if board[2][2] and board[1][1] and board[0][0] or board[2][0] and board[1][1] and board[0][2]:
+        print("-----------------------------")
+        print("-----------------------------")
+        print("-----------------------------")
+        print("    HAS GANADO EL JUEGO   ")
+        print("-----------------------------")
+        print("-----------------------------")
+        print("-----------------------------")
+                
+           
+    
+
 
 
 '''
 Testing: 
 '''
+
 board = create_empty_board()
 print_board(board)
+x=int(input("Ingrese el numero en coordenada x:      ")) 
+y=int(input("Ingrese el numero en coordenada y:      "))
+player="O"
+
+update_board(board,x,y,player)
+print_board(board)
+
+x=int(input("Ingrese el numero en coordenada x:      ")) 
+y=int(input("Ingrese el numero en coordenada y:      "))
+player="O"
+
+update_board(board,x,y,player)
+print_board(board)
+
+x=int(input("Ingrese el numero en coordenada x:      ")) 
+y=int(input("Ingrese el numero en coordenada y:      "))
+player="O"
+
+update_board(board,x,y,player)
+print_board(board)
+
+w=check_for_winner(board)
